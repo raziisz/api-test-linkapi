d020c5795fab2febaf756311df378ccbf0fb7653
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-12-02 13:00:41 -0400

    config env and connection

---
+++ src/database/connection.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const URL_CONNECTION = process.env.MONGO_URL;
+
+export const connect = async () => {
+  await mongoose.connect(URL_CONNECTION, {
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+}
+
+export const disconnect = async () => mongoose.connection.close();
